fccde21ea91966b17d2bfef874e5324ac4191141
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-08-27 22:08:16 +0530

    dashboard

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,9 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"],variable: "--font-sans", });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+    <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
